58db9132c29a7946e9c2cd1134ae915362a82055
littlebusters <little.busters.reply@gmail.com>
2015-08-21 11:05:08 +0900

    bugfix

--- Sketch CSS Sprite Mixin.sketchplugin/Contents/Sketch/net.creative-tweet.library.js
+++ Sketch CSS Sprite Mixin.sketchplugin/Contents/Sketch/net.creative-tweet.library.js
@@ -1,14 +1,16 @@
-var doc = context.document;
 var cssSpriteGenerator = cssSpriteGenerator || {};
-var artboard = [[doc currentPage] currentArtboard];
-if ( !artboard ) {
-	artboard = [[[doc currentPage] artboards] objectAtIndex:0];
+
+cssSpriteGenerator.init = function( context ) {
+	this.context = context;
+	this.doc = context.document;
+	this.artboard = this.doc.currentPage().currentArtboard();
+	if( !this.artboard ) this.artboard = this.doc.currentPage().artboards().objectAtIndex( 0 );
+
+	this.pasteBoard = null;
+	this.pasteBoard = NSPasteboard.generalPasteboard();
+	this.pasteBoard.declareTypes_owner( [ NSPasteboardTypeString ], null );
 }
 
-cssSpriteGenerator.pluginVersion = function ( context ) {
-	log( 'Version: 1.1.0' );
-	context.document.showMessage( 'Version: 1.1.0' );
-}
 cssSpriteGenerator.getMixinSet = function( _type ) {
 	return this.getMixin( _type ) + this.getSpriteValue( _type )
 }
@@ -63,58 +65,28 @@ cssSpriteGenerator.getMixin = function( _type ) {
 			break;
 	}
 
-	var terminate = ';';
-	if( _type == 'sass' ) terminate = ''; 
-	
+	var termination = ';';
+	if( _type == 'sass' ) termination = ''; 
+	// @media only screen and (-webkit-min-device-pixel-ratio: 2), 
+	// only screen and (min-device-pixel-ratio: 2) {
 	var mixin  = mixinStart
-			   + '\twidth:' + wrapStart + varName + listIdx + ( i++ ) + wrapEnd + terminate + '\n'
-			   + '\theight:' + wrapStart + varName + listIdx + ( i++ ) + wrapEnd + terminate + '\n'
+			   + '\twidth:' + wrapStart + varName + listIdx + ( i++ ) + wrapEnd + termination + '\n'
+			   + '\theight:' + wrapStart + varName + listIdx + ( i++ ) + wrapEnd + termination + '\n'
 			   + '\tbackground-repeat: no-repeat;\n'
 			   + '\tbackground-image:' + bgImageStart 
-			   						   + wrapStart + varName + listIdx + ( i++ ) + wrapEnd 
-			   						   + bgImageEnd
+									   + wrapStart + varName + listIdx + ( i++ ) + wrapEnd 
+									   + bgImageEnd
 			   + '\tbackground-position:' + wrapStart + varName + listIdx + ( i++ ) + wrapEnd
-										  + wrapStart + varName + listIdx + i + wrapEnd + terminate + '\n'
+										  + wrapStart + varName + listIdx + i + wrapEnd + termination + '\n'
 			   + mixinEnd;
 
 	return mixin;
 }
 
-cssSpriteGenerator.getExportDir = function() {
-	var fileURL  = [doc fileURL],
-		filePath = [fileURL path],
-		fileDir  = filePath.split( [doc displayName] )[0];
-
-	var exportDir = ( [artboard name] ).split( ':' )[1];
-
-	if ( exportDir ) {
-
-	} else {
-		var openPanel = [NSOpenPanel openPanel];
-			[openPanel setPrompt:@"Choose"];
-			[openPanel setTitle:@"Choose an Export Directory"];
-			[openPanel setCanCreateDirectories:true];
-			[openPanel setCanChooseFiles:false];
-			[openPanel setCanChooseDirectories:true];
-			[openPanel setAllowsMultipleSelection:false];
-			[openPanel setShowsHiddenFiles:false];
-			[openPanel setExtensionHidden:false];
-			[openPanel setDirectoryURL:[NSURL fileURLWithPath:"~/Documents/"]];
-		[[NSApplication sharedApplication] activateIgnoringOtherApps:true];
-		var openPanelButtonPressed = [openPanel runModal];
-		if (openPanelButtonPressed == NSFileHandlingPanelOKButton) {
-			exportDir = [[openPanel URL] path];
-			print( exportDir );
-			// return exportDir
-		}
-	}
-}
-
-
 cssSpriteGenerator.getSpriteValue = function( _type ) {
 	var prefix = '',
 		sepalator = '',
-		terminate = ';';
+		termination = ';';
 
 	switch ( _type ) {
 		case 'scss':
@@ -132,30 +104,30 @@ cssSpriteGenerator.getSpriteValue = function( _type ) {
 		default:
 			prefix = '$';
 			sepalator = ':';
-			terminate = '';
+			termination = '';
 			break;
 	}
 
-	var layers = [artboard layers],
+	var layers = this.artboard.layers(),
 		spriteVariable = '',
-		imageName      = [artboard name];
+		imageName      = this.artboard.name();
 		imagePath      = '../img/' + imageName + '.png',
 		imagePathx2    =  '../img/' + imageName + '@2.png',
 
-	spritePathVariableName   = prefix + [artboard name] + '-path';
-	spritePathVariableNamex2 = spritePathVariableName + '@2';
+	spritePathVariableName   = prefix + this.artboard.name() + '-path';
+	spritePathVariableNamex2 = spritePathVariableName + 'x2';
 	spriteVariable += spritePathVariableName + sepalator + ' \'' + imagePath + '\';\n';
 	spriteVariable += spritePathVariableNamex2 + sepalator + ' \'' + imagePathx2 + '\';\n';
 
-	for (var i = [layers count] - 1; i >= 0; i--) {
-		var layer = [layers objectAtIndex:i];
-			spriteVariable += prefix + [layer name] + sepalator
-						    + ' ' + [[layer frame] width] + 'px' 
-						    + ' ' + [[layer frame] height] + 'px'
-						    + ' ' + spritePathVariableName
-						    + ' ' + ( 0 - [[layer frame] x] ) + 'px' 
-						    + ' ' + ( 0 - [[layer frame] y] ) + 'px' 
-						    + ';\n';
+	for (var i = layers.count() - 1; i >= 0; i--) {
+		var layer = layers.objectAtIndex( i );
+			spriteVariable += prefix + layer.name() + sepalator
+							+ ' ' + layer.frame().width() + 'px' 
+							+ ' ' + layer.frame().height() + 'px'
+							+ ' ' + spritePathVariableName
+							+ ' ' + ( 0 - layer.frame().x() ) + 'px' 
+							+ ' ' + ( 0 - layer.frame().y() ) + 'px' 
+							+ ';\n';
 	};
 
 	return spriteVariable;
